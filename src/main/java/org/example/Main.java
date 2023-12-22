233366d4ae0678cb42554a9e917fcee09f5a1b3f
Yaroslav-Korzh <dunny04@gmail.com>
2023-12-22 10:02:42 +0100

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int palindrome = -121;
+        int eqNumber = 0;
+        int zikl = palindrome;
+
+        while (zikl != 0) {
+            int remainder = zikl % 10;
+            zikl = zikl / 10;
+            eqNumber = eqNumber * 10 + remainder;
+        }
+
+        if (palindrome < 0 || palindrome != eqNumber) {
+            System.out.println("false");
+        } else System.out.println("true");
+
     }
 }
